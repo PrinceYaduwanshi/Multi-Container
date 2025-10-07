38938287d532d6f733670e0b1aec8eb68a4997d8
PrinceYaduwanshi <rajprince9250@gmail.com>
2025-10-07 16:56:49 +0530

    multi-container-commit-1

---
+++ client/src/OtherPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const OtherPage = () => {
+  return (
+    <div>
+      In some other page which is changed!
+      <Link to="/">Go back home</Link>
+    </div>
+  );
+};
+
+export default OtherPage;
