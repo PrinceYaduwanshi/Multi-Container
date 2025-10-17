abb76169df9309a0e5a4724874332acf61ec466c
PrinceYaduwanshi <rajprince9250@gmail.com>
2025-10-17 06:39:47 +0530

    error changed

--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const OtherPage = () => {
   return (
     <div>
-      In some other page which is changed!
+      In some other page!
       <Link to="/">Go back home</Link>
     </div>
   );
